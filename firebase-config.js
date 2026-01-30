fd87c2923dc1cbb04d08d2568d5457087f2322ce
IlyaSigma111 <ilyagulkov25@gmail.com>
2026-01-30 18:27:05 +0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -11,56 +11,7 @@ import {
     updateProfile
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
-// Конфигурация Firebase (ВАША)
-const firebaseConfig = {
-    apiKey: "AIzaSyDxGOGD6Oooo1CILrmrTpzy5Sq_MPuGiKM",
-    authDomain: "messenger-4a3ab.firebaseapp.com",
-    databaseURL: "https://messenger-4a3ab-default-rtdb.firebaseio.com",
-    projectId: "messenger-4a3ab",
-    storageBucket: "messenger-4a3ab.firebasestorage.app",
-    messagingSenderId: "684785124123",
-    appId: "1:684785124123:web:15efc74d7bb49259b789be"
-};
-
-// Инициализация Firebase
-const app = initializeApp(firebaseConfig);
-
-// Инициализация сервисов
-const database = getDatabase(app);
-const auth = getAuth(app);
-const googleProvider = new GoogleAuthProvider();
-
-// Экспорт всего необходимого
-export { 
-    database, 
-    ref, 
-    push, 
-    onValue, 
-    set, 
-    get, 
-    child, 
-    auth, 
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    signInWithPopup,
-    googleProvider,
-    onAuthStateChanged, 
-    signOut,
-    updateProfile
-};import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getDatabase, ref, push, onValue, set, get, child } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
-import { 
-    getAuth, 
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    signInWithPopup,
-    GoogleAuthProvider,
-    onAuthStateChanged,
-    signOut,
-    updateProfile
-} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-
-// Конфигурация Firebase (ВАША)
+// Конфигурация Firebase
 const firebaseConfig = {
     apiKey: "AIzaSyDxGOGD6Oooo1CILrmrTpzy5Sq_MPuGiKM",
     authDomain: "messenger-4a3ab.firebaseapp.com",
@@ -97,4 +48,3 @@ export {
     signOut,
     updateProfile
 };
-
