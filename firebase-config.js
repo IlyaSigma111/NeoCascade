b5ab1696e2a3b74253fcf2d893c66cd092713a5c
IlyaSigma111 <ilyagulkov25@gmail.com>
2026-01-30 17:13:12 +0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getDatabase, ref, push, onValue, set, get, child } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
-import { getAuth, signInAnonymously, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { 
+    getAuth, 
+    signInWithPopup,
+    GoogleAuthProvider,
+    onAuthStateChanged,
+    signOut 
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
+// Конфигурация Firebase
 const firebaseConfig = {
     apiKey: "AIzaSyDxGOGD6Oooo1CILrmrTpzy5Sq_MPuGiKM",
     authDomain: "messenger-4a3ab.firebaseapp.com",
@@ -12,8 +19,26 @@ const firebaseConfig = {
     appId: "1:684785124123:web:15efc74d7bb49259b789be"
 };
 
+// Инициализация Firebase
 const app = initializeApp(firebaseConfig);
+
+// Инициализация сервисов
 const database = getDatabase(app);
 const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
-export { database, ref, push, onValue, set, get, child, auth, signInAnonymously, onAuthStateChanged };
+// Экспорт всего необходимого
+export { 
+    database, 
+    ref, 
+    push, 
+    onValue, 
+    set, 
+    get, 
+    child, 
+    auth, 
+    signInWithPopup, 
+    googleProvider, 
+    onAuthStateChanged, 
+    signOut 
+};
