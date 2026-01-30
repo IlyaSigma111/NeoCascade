314a678b3a175bdcdf378723f7c0094876498c76
IlyaSigma111 <ilyagulkov25@gmail.com>
2026-01-30 16:55:03 +0500

    Update firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getDatabase, ref, push, onValue, set, get, child } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+import { getAuth, signInAnonymously, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDxGOGD6Oooo1CILrmrTpzy5Sq_MPuGiKM",
+    authDomain: "messenger-4a3ab.firebaseapp.com",
+    databaseURL: "https://messenger-4a3ab-default-rtdb.firebaseio.com",
+    projectId: "messenger-4a3ab",
+    storageBucket: "messenger-4a3ab.firebasestorage.app",
+    messagingSenderId: "684785124123",
+    appId: "1:684785124123:web:15efc74d7bb49259b789be"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const auth = getAuth(app);
+
+export { database, ref, push, onValue, set, get, child, auth, signInAnonymously, onAuthStateChanged };
