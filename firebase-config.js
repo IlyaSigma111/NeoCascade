f3b44fb7afb8acfd6ddcc5b544a80e93770fa9fa
IlyaSigma111 <ilyagulkov25@gmail.com>
2026-02-04 18:34:05 +0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getDatabase, ref, push, onValue, set, get, child, update, remove } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+import { getDatabase, ref, push, onValue, set, get, child } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 import { 
     getAuth, 
     createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
     signInWithPopup,
     GoogleAuthProvider,
-    GithubAuthProvider,
     onAuthStateChanged,
     signOut,
-    updateProfile,
-    updateEmail,
-    updatePassword
+    updateProfile
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-import { getStorage, ref as storageRef, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
 
 // Конфигурация Firebase
 const firebaseConfig = {
@@ -23,8 +19,7 @@ const firebaseConfig = {
     projectId: "messenger-4a3ab",
     storageBucket: "messenger-4a3ab.firebasestorage.app",
     messagingSenderId: "684785124123",
-    appId: "1:684785124123:web:15efc74d7bb49259b789be",
-    measurementId: "G-MY6Z7YB5J9"
+    appId: "1:684785124123:web:15efc74d7bb49259b789be"
 };
 
 // Инициализация Firebase
@@ -33,41 +28,27 @@ const app = initializeApp(firebaseConfig);
 // Инициализация сервисов
 const database = getDatabase(app);
 const auth = getAuth(app);
-const storage = getStorage(app);
-
-// Провайдеры
 const googleProvider = new GoogleAuthProvider();
-const githubProvider = new GithubAuthProvider();
 
-// Настройки провайдеров
-googleProvider.addScope('email');
+// Настраиваем Google провайдер
 googleProvider.addScope('profile');
-githubProvider.addScope('user:email');
+googleProvider.addScope('email');
 
-// Экспорт всего необходимого
+// Экспорт
 export { 
     database, 
-    storage,
     ref, 
-    storageRef,
     push, 
     onValue, 
     set, 
     get, 
-    child,
-    update,
-    remove,
-    uploadBytes,
-    getDownloadURL,
+    child, 
     auth, 
     createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
     signInWithPopup,
     googleProvider,
-    githubProvider,
     onAuthStateChanged, 
     signOut,
-    updateProfile,
-    updateEmail,
-    updatePassword
+    updateProfile
 };
