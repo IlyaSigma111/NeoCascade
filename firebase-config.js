7446de03f671bca816a403850dd3c2892a46980e
IlyaSigma111 <ilyagulkov25@gmail.com>
2026-02-04 18:22:30 +0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,19 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getDatabase, ref, push, onValue, set, get, child } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+import { getDatabase, ref, push, onValue, set, get, child, update, remove } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 import { 
     getAuth, 
     createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
     signInWithPopup,
     GoogleAuthProvider,
+    GithubAuthProvider,
     onAuthStateChanged,
     signOut,
-    updateProfile
+    updateProfile,
+    updateEmail,
+    updatePassword
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getStorage, ref as storageRef, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
 
 // Конфигурация Firebase
 const firebaseConfig = {
@@ -19,7 +23,8 @@ const firebaseConfig = {
     projectId: "messenger-4a3ab",
     storageBucket: "messenger-4a3ab.firebasestorage.app",
     messagingSenderId: "684785124123",
-    appId: "1:684785124123:web:15efc74d7bb49259b789be"
+    appId: "1:684785124123:web:15efc74d7bb49259b789be",
+    measurementId: "G-MY6Z7YB5J9"
 };
 
 // Инициализация Firebase
@@ -28,23 +33,41 @@ const app = initializeApp(firebaseConfig);
 // Инициализация сервисов
 const database = getDatabase(app);
 const auth = getAuth(app);
+const storage = getStorage(app);
+
+// Провайдеры
 const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
+
+// Настройки провайдеров
+googleProvider.addScope('email');
+googleProvider.addScope('profile');
+githubProvider.addScope('user:email');
 
 // Экспорт всего необходимого
 export { 
     database, 
+    storage,
     ref, 
+    storageRef,
     push, 
     onValue, 
     set, 
     get, 
-    child, 
+    child,
+    update,
+    remove,
+    uploadBytes,
+    getDownloadURL,
     auth, 
     createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
     signInWithPopup,
     googleProvider,
+    githubProvider,
     onAuthStateChanged, 
     signOut,
-    updateProfile
+    updateProfile,
+    updateEmail,
+    updatePassword
 };
